4c6f42334634cdcc3e9ef42b03a2d8c98907ecf5
Harrison Jones <hwjones681@gmail.com>
2019-04-20 17:01:03 -0400

    Add class EnigmaReflector which implements the reflector functionality in the Enigma machine

---
+++ Enigma_Console/EnigmaReflector.cs
@@ -0,0 +1,26 @@
+﻿namespace Enigma_Console
+{
+    class EnigmaReflector
+    {
+        /* The reflector in an Enigma machine connects pairs of letters together, e.g. if 'A' is wired to 'H', then
+         * inputting 'A' into the reflector will give 'H' as an output, and 'H' will give 'A'.*/
+        private readonly int[] output;
+        
+        public EnigmaReflector(int[][] pairings)
+        {
+            /* In order to ensure that the reflector is constructed with the correct behavior, it is instantiated with a
+             * 2-dimensional array of pairings, e.g. [[1, 12], [2, 9] ...] */
+            
+            foreach (int[] pairing in pairings)
+            {
+                output[pairing[0]] = pairing[1];
+                output[pairing[1]] = pairing[0];
+            }
+        }
+
+        public int GetReflectorOutput(int input)
+        {
+            return this.output[input];
+        }
+    }
+}
