a0f212ea0f50e0f08d37cdcbcd9460644a40b7d8
Harrison Jones <hwjones681@gmail.com>
2019-10-11 07:55:25 -0600

    EnigmaReflector:     Refactor to use characters instead of integers

--- Enigma_Console/EnigmaReflector.cs
+++ Enigma_Console/EnigmaReflector.cs
@@ -1,26 +1,28 @@
-﻿namespace Enigma_Console
+﻿using System.Collections.Generic;
+
+namespace Enigma_Console
 {
     class EnigmaReflector
     {
         /* The reflector in an Enigma machine connects pairs of letters together, e.g. if 'A' is wired to 'H', then
          * inputting 'A' into the reflector will give 'H' as an output, and 'H' will give 'A'.*/
-        private readonly int[] output;
-        
-        public EnigmaReflector(int[][] pairings)
+        private Dictionary<char, char> outputMapping;
+        private readonly static int ALPHABET_LENGTH = 26;
+        private readonly static int CAPITAL_LETTER_CHAR_OFFSET = 65;
+
+        public EnigmaReflector(char[] outputOrder)
         {
-            /* In order to ensure that the reflector is constructed with the correct behavior, it is instantiated with a
-             * 2-dimensional array of pairings, e.g. [[1, 12], [2, 9] ...] */
-            
-            foreach (int[] pairing in pairings)
+            this.outputMapping = new Dictionary<char, char>();
+            for (int i = 0; i < ALPHABET_LENGTH; i += 1)
             {
-                this.output[pairing[0] - 1] = pairing[1];
-                this.output[pairing[1] - 1] = pairing[0];
+                char currentLetter = (char)(CAPITAL_LETTER_CHAR_OFFSET + i);
+                this.outputMapping.Add(currentLetter, outputOrder[i]);
             }
         }
 
-        public int GetReflectorOutput(int input)
+        public char GetReflectorOutput(char input)
         {
-            return this.output[input - 1];
+            return this.outputMapping[input];
         }
     }
 }
